8b32d0ab7f915a6c9db680d9445145a94bd39a03
Daniel Esteban <conejo@conejo.me>
2020-01-31 16:43:50 +0100

    wip

--- flappy.go
+++ flappy.go
@@ -32,13 +32,14 @@ func Flappy() {
 		{false, false, false, false, true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false},
 		{false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
 	}
-	gopherBlack := make([]color.RGBA, 462)
-	for i := 0; i < 21; i++ {
-		for j := 0; j < 22; j++ {
+	var gopherBlack [22][]color.RGBA
+	for j := 0; j < 22; j++ {
+		gopherBlack[j] = make([]color.RGBA, 21)
+		for i := 0; i < 21; i++ {
 			if gopherBalloon[i][j] {
-				gopherBlack[22*i+j] = colors[BLACK]
+				gopherBlack[j][i] = colors[BLACK]
 			} else {
-				gopherBlack[22*i+j] = colors[SKY]
+				gopherBlack[j][i] = colors[SKY]
 			}
 		}
 	}
@@ -54,9 +55,11 @@ func Flappy() {
 	var gx int16
 	var gy int16
 	var lastGy int16
+	var k int16
 	speedY := float32(-1)
 	gy = 60
 	lastGy = 60
+	gPressed := false
 	for {
 
 		pressed, _ = buttons.Read8Input()
@@ -67,11 +70,18 @@ func Flappy() {
 		}
 
 		if pressed&machine.BUTTON_A_MASK > 0 || pressed&machine.BUTTON_B_MASK > 0 {
-			speedY -= 2
+			if !gPressed {
+				speedY -= 4
+				gPressed = true
+			} else {
+				speedY -= GRAVITY * 0.6
+			}
+		} else {
+			gPressed = false
 		}
 
-		flappyResetLine(159 - i + 1)
-		if i > 10 && i < 20 {
+		flappyResetLine(159 - i)
+		if (i > 10 && i < 20) || (i > 140 && i < 150) {
 			flappyDrawTube(159 - i)
 		}
 
@@ -84,13 +94,40 @@ func Flappy() {
 		if speedY > 1.4 {
 			speedY = 1.4
 		}
-		if speedY < -1.4 {
-			speedY = -1.4
+		gy = gy + int16(speedY)
+		if speedY < -3 {
+			speedY = -3
 		}
 
-		gy = gy + int16(speedY)
-		display.FillRectangle(gx-1, lastGy, 22, 21, colors[SKY])
-		display.FillRectangleWithBuffer(gx, gy, 22, 21, gopherBlack)
+		if gy < 0 {
+			gy = 0
+		}
+		if gy > 86 {
+			gy = 86
+		}
+
+		gx -=1
+		if gx <0 {
+			gx+=159
+		}
+		for k = 0; k < 22; k++ {
+			display.FillRectangle(gx, lastGy, 1, 21, colors[SKY])
+			gx++
+			if gx > 159 {
+				gx -= 159
+			}
+		}
+		gx -= 21
+		if gx < 0 {
+			gx += 159
+		}
+		for lastGy = 0; lastGy < 22; lastGy++ {
+			display.FillRectangleWithBuffer(gx, gy, 1, 21, gopherBlack[lastGy])
+			gx++
+			if gx > 159 {
+				gx -= 159
+			}
+		}
 		//tinyfont.DrawChar(&display, &fonts.Regular32pt, gx-1, lastGy, byte('E'), colors[SKY])
 		//tinyfont.DrawChar(&display, &fonts.Regular32pt, gx, gy, byte('E'), colors[BLACK])
 		//display.FillRectangle(gx-1, lastGy, 14, 14, colors[SKY])
@@ -102,7 +139,7 @@ func Flappy() {
 		if i < 0 {
 			i = 159
 		}
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(4 * time.Millisecond)
 	}
 	display.SetScroll(0)
 	display.StopScroll()
@@ -112,6 +149,7 @@ func flappyResetLine(i int16) {
 	display.FillRectangle(i, 0, 1, 108, colors[SKY])
 	display.FillRectangle(i, 108, 1, 2, colors[DARKBROWN])
 	display.FillRectangle(i, 110, 1, 18, colors[BROWN])
+	display.FillRectangle(i+1, 0, 1, 128, colors[RED])
 }
 
 func flappyDrawTube(i int16) {
